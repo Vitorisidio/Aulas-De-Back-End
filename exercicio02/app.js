c696b8c6316a4a5e70c84e3ce7073dec5fb284c1
VITOR ISIDIO FEITOSA DA SILVA <25203658@CFP127ALUNO.LOCAL>
2026-02-06 10:51:38 -0300

    exercicio da aula 2 concluido

--- exercicio02/app.js
+++ exercicio02/app.js
@@ -9,65 +9,117 @@ const entradaDeDados = readline.createInterface({
 })
 
 
-entradaDeDados.question('por favor digite o seu nome:', function (nomeUsuario) {
+entradaDeDados.question('por favor digite o seu nome:', function (nomeUsuario) {// abre nomeUsuario
 
-    entradaDeDados.question('por favor digite o nome do produto:', function (nomeProduto) {
+    entradaDeDados.question('por favor digite o nome do produto:', function (nomeProduto) { // abre nomeProduto
 
-        if (!isNaN(nomeProduto)) {
-            console.log("Digite Um produto da loja")
-            entradaDeDados.close()
-            return
-        }
-        entradaDeDados.question('por favor digite o valor da compra: ', function (ValorDaCompra) {
+        entradaDeDados.question('por favor digite o valor da compra: ', function (ValorDaCompra) { // Abre ValorDaCompra
 
-            if (isNaN(ValorDaCompra)) {
-                console.log("Digite um número válido para o valor da compra!")
-                entradaDeDados.close()
-                return
-            } else if (Number(ValorDaCompra) <= 0) {
-                console.log("Digite um número maior que ZERO")
-                entradaDeDados.close()
-                return
-            }
+            entradaDeDados.question('por favor digite o taxa de juros ', function (taxaDeJuros) {// abre taxaDeJuros
 
-            entradaDeDados.question('por favor digite o taxa de juros ', function (taxaDeJuros) {
+                entradaDeDados.question('por favor digite 1 para informar em mêses ou 2 para informar em anos: ', function (tempo) { //abre tempo
 
-                if (isNaN(taxaDeJuros) || Number(taxaDeJuros) < 0 || Number(taxaDeJuros) > 100) {
-                    console.log("Taxa de juros inválida!")
-                    entradaDeDados.close()
-                    return
-                }
-                let taxa = Number(taxaDeJuros) / 100;
-                console.log(taxa)
+                    let taxa = Number(taxaDeJuros) / 100;// calculo da %
 
+                    //---------------------------------------------------------TRATAMENTO DE ERROS---------------------------------//
 
-                entradaDeDados.question('por favor digite a quantidade de parcelas: ', function (parcelas) {
-
-                    if (isNaN(parcelas) || Number(parcelas) <= 0) {
-                        console.log("Tempo de pagamento inválido!")
+                    if (!isNaN(nomeProduto)) {
+                        console.log("Digite Um produto da loja")
                         entradaDeDados.close()
-                        return
+                
                     }
-                    let valorFinal = Number(ValorDaCompra) * ((1 + Number(taxa)) ** parcelas)
-                    console.log(valorFinal)
 
-                    let valorParcelas = Number(valorFinal) / Number(parcelas)
-                    let acrescimo = valorFinal - ValorDaCompra
+                    if (isNaN(ValorDaCompra)) {
+                        console.log("Digite um número válido para o valor da compra!")
+                        entradaDeDados.close()
+                        
+                    } else if (Number(ValorDaCompra) <= 0) {
+                        console.log("Digite um número maior que ZERO")
+                        entradaDeDados.close()
+                        
+                    }
+
+                    if (isNaN(taxaDeJuros) || Number(taxaDeJuros) < 0 || Number(taxaDeJuros) > 100) {
+                        console.log("Taxa de juros inválida!")
+                        entradaDeDados.close()
+                        
+                    }
+                    
+                    if (isNaN(tempo)||tempo <= 0 || tempo > 2) {
+                        console.log("Valor Invalido, escolha apenas 1 ou 2")
+                        entradaDeDados.close()
+                        
+                        //---------------------------------------------------------LOGICA------------------------------------------------------//
+
+                        //---------------------------------------------------------LOGICA meses------------------------------------------------------//
+
+                    } else if (tempo == 1) {
+                        
+
+                        entradaDeDados.question('por favor digite a quantidade de parcelas EM MESES: ', function (parcelas/*a quantidade de parcelas em meses*/) {
+
+
+                            if (isNaN(parcelas) || Number(parcelas) <= 0) {
+                                console.log("valor de pagamento inválido!")
+                                entradaDeDados.close()
+                                return
+                            }//trtativa de erro
+                            let valorFinal = Number(ValorDaCompra) * ((1 + Number(taxa)) ** parcelas)// calculo do produto com a juros
+
+                            let valorParcelas = Number(valorFinal) / Number(parcelas)// conta do valor das parcelas
+                            let acrescimo = valorFinal - ValorDaCompra // quanto de juros foi add no valor da compra
 
 
 
-                    console.log("----------------------" + empresa + "---------------------------")
-                    console.log("Muito obrigado por realizar a sua compra conosco Sr(a): " + nomeUsuario)
-                    console.log("A compra do produto: " + nomeProduto + " Tem um valor de: " + ValorDaCompra)
-                    console.log("A sua compra será parcelada em: " + parcelas + " vezes e o Sr(a) pagará: " + valorParcelas.toFixed(2))
-                    console.log("O acréscimo realizado ao valor de: " + ValorDaCompra + "será de: " + acrescimo.toFixed(2))
-                    console.log("Muito obrigado por escolher a " + empresa)
-                    console.log("-----------------------------------------------------------------")
+                            console.log("----------------------" + empresa + "---------------------------")
+                            console.log("Muito obrigado por realizar a sua compra conosco Sr(a): " + nomeUsuario)
+                            console.log("A compra do produto: " + nomeProduto + " Tem um valor de: " + ValorDaCompra + " REAIS")
+                            console.log("A sua compra será parcelada em: " + parcelas + " vezes e o Sr(a) pagará: " + valorParcelas.toFixed(2))
+                            console.log("O acréscimo realizado ao valor de: " + ValorDaCompra + " REAIS será de: " + acrescimo.toFixed(2) + " REAIS")
+                            console.log("VALOR TOTAL É DE " + valorFinal)
+                            console.log("Muito obrigado por escolher a " + empresa)
+                            console.log("-----------------------------------------------------------------")
 
-                })
+                            entradaDeDados.close()
+                            return
+                        })
 
-            })
+                        //---------------------------------------------------------LOGICA anos------------------------------------------------------//
 
-        })
-    })
-})
+                    } else if (tempo == 2) {
+
+
+                        entradaDeDados.question('por favor digite a quantidade de parcelas EM ANOS: ', function (parcelas /*a quantidade de parcelas em anos*/) {
+
+                         convercacao = parcelas*12 // converter anos em meses
+
+                            if (isNaN(parcelas) || Number(parcelas) <= 0) {
+                                console.log("valor de pagamento inválido!")
+                                entradaDeDados.close()
+                                return
+                            }//trtativa de erro
+                            let valorFinal = Number(ValorDaCompra) * ((1 + Number(taxa)) ** (convercacao))// calculo do produto com a juros
+
+                            let valorParcelas = Number(valorFinal) / Number(convercacao)// conta do valor das parcelas
+                            let acrescimo = valorFinal - ValorDaCompra// quanto de juros foi add no valor da compra
+
+
+
+                            console.log("----------------------" + empresa + "---------------------------")
+                            console.log("Muito obrigado por realizar a sua compra conosco Sr(a): " + nomeUsuario)
+                            console.log("A compra do produto: " + nomeProduto + " Tem um valor de: " + ValorDaCompra + " REAIS")
+                            console.log("A sua compra será parcelada em: " + convercacao + " vezes e o Sr(a) pagará: " + valorParcelas.toFixed(2))
+                            console.log("O acréscimo realizado ao valor de: " + ValorDaCompra + "REAIS será de: " + acrescimo.toFixed(2) + " REAIS")
+                            console.log("VALOR TOTAL É DE " + valorFinal)
+                            console.log("Muito obrigado por escolher a " + empresa)
+                            console.log("-----------------------------------------------------------------")
+
+                            entradaDeDados.close()
+                            return
+                        })
+                    }
+                })//fecha tempo
+            })// fecha taxaDeJuros
+        })// fecha ValorDaCompra
+    })// fecha nomeProduto
+})// fecha nomeUsuario
