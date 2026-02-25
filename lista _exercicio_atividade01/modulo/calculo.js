edb6badb5a50891ed0225bd3729a54854eee48b0
VITOR ISIDIO FEITOSA DA SILVA <25203658@CFP127ALUNO.LOCAL>
2026-02-25 11:27:40 -0300

    iniciando atividade

---
+++ lista _exercicio_atividade01/modulo/calculo.js
@@ -0,0 +1,8 @@
+const calcular = function (nomeAluno, pesoAluno, AlturaAluno) {
+
+let nome = String(nomeAluno)
+let peso = Number(pesoAluno)
+let altura = Number(AlturaAluno)
+}
+
+const imc = (pesoAluno, AlturaAluno) => Number(pesoAluno) / (Number(AlturaAluno) * Number(AlturaAluno))
