ff71073d60228acc4ba50deefc2e9469ef21bdda
VITOR ISIDIO FEITOSA DA SILVA <25203658@CFP127ALUNO.LOCAL>
2026-03-05 08:58:53 -0300

    lógica da tabuada concluida

---
+++ lista_exercicio/lista_atividade03/modulo/calculo.js
@@ -0,0 +1,21 @@
+const calcular = function (tabuadaInicial, tabuadaFinal, numeroInicial, numeroFinal) {
+    let tabInicial = Number(tabuadaInicial)
+    let tabFinal = Number(tabuadaFinal)
+    let numInicial = Number(numeroInicial)
+    let numFinal = Number(numeroFinal)
+    let resultado
+
+    for (tabInicial; tabInicial <= tabFinal; tabInicial++) {
+
+        console.log("\n")
+        for ( let reset = numInicial ; reset <= numFinal; reset++) {
+
+            resultado = tabInicial * reset
+            console.log(tabInicial + " x " + reset + ' = ' + resultado  )
+
+        }
+
+    }
+
+}
+calcular(1, 10, 0, 10)
