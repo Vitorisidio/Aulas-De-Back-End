b5cd04f3d0965008e56b39d0aa2bb6dbaa3ec756
VITOR ISIDIO FEITOSA DA SILVA <25203658@CFP127ALUNO.LOCAL>
2026-04-17 11:33:44 -0300

    config do model, do controller e do app

---
+++ aula06/app.js
@@ -0,0 +1,48 @@
+/*********************************
+ * 
+ * 
+ * 
+*********************************/
+
+
+//import das dependencias para criar a API
+const express = require('express')
+const cors = require('cors')
+const bodyParse = require('body-parser')
+
+//import das controllers do projeto
+const controllerFilme = require('./controller/filme/controller_filme.js')
+
+//Criando um objeto para manipular dados do body da API em formato JSON
+const bodyParseJSON = bodyParse.json()
+
+//Criando um objeto para manipular o express
+const app = express()
+
+//conjunto de permissões a serem aplicadas no CORS da API
+const corsOptions = {
+    origin: ['*'], //Origem da requisição, podendo ser um ip(so maquinas especificas podem acessar a API) ou o *(todas as maquinas podem acessar a API)
+    methods: 'GET, POST, PUT, DELETE, OPTIONS', //São os verbos que serão liberados na API (GET, POST, PUT e DELETE)
+    allowedHeaders: ['Content-type', 'Autorization'], // São permissões de cabeçalho do CORS
+}
+
+//configura as permissões da API através do CORS
+app.use(cors(corsOptions))
+
+//ENDPOINTS
+app.post('/v1/senai/locadora/filme', bodyParseJSON, async function (request, response) {
+    //recebe o conteúdo dentro do body da requisição
+    let dados = request.body
+
+    let result = await controllerFilme.inserirNovoFilme(dados)
+
+    response.status(result.status_code)
+    response.json(result)
+})
+
+
+
+//serve para inicializar a Api para receber requisições
+app.listen(8080, function () {
+    console.log('Api funcionando e aguardando novas requisições...')
+})
